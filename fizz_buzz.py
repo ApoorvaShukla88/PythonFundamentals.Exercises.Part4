498bc52387a4ceb2e61444d62f684953138388da
Apoorva Mishra <apoorvashukla88@gmail.com>
2020-03-05 22:45:37 -0500

    FIZZBUZZ

---
+++ fizz_buzz.py
@@ -0,0 +1,14 @@
+
+
+
+
+for number in range(1, 100):
+        print(str(number))
+        if number % 3 == 0:
+                print("FIZZ")
+        elif number % 5 == 0:
+                print("BUZZ")
+        elif number % 5 == 0 and number % 3 == 0:
+                print("FizzBuzz")
+
+
